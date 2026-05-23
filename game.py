1557f6b1025e0893623d064eab9adfca94b7112a
ProjectSTOPe <dpgmir4@gmail.com>
2026-05-23 22:45:27 +0300

    game.py

--- game.py
+++ game.py
@@ -1,32 +1,28 @@
 import os
 import telebot
-import sqlite3
-import random
-import time
-import requests
-from telebot import types
 from flask import Flask
 from threading import Thread
 
+# Твой токен
 TOKEN = '8840112637:AAHKDM7xiUQlw9c4o_z79dTeIqs4jJtWLVc'
 bot = telebot.TeleBot(TOKEN)
 app = Flask('')
-
-# --- ТУТ БУДУТ ТВОИ ФУНКЦИИ (init_db, CLASSES, и т.д.) ---
-# Оставь всё, что было у тебя раньше (init_db, CLASSES, GRADES, LOOT_POOL, хэндлеры...)
 
 @app.route('/')
 def home():
     return "Bot is alive"
 
+# Простейший обработчик
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Связь работает!")
+
 def run_web():
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
 if __name__ == '__main__':
-    # Запускаем веб-сервер в фоне
     t = Thread(target=run_web)
     t.start()
-
-    # Запускаем бота
-    print("Бот запускается...")
-bot.infinity_polling(none_stop=True, interval=0, timeout=20)
+    print("Бот запущен, жду команды /start...")
+    bot.infinity_polling()
+    
